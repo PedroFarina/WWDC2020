6593f27b5df8004eaf3f0fa4dbefb6bca54eff5e
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-05-16 20:04:33 -0300

    Shooting component

--- WWDC 2020.playground/Sources/Components/ShootingComponent.swift
+++ WWDC 2020.playground/Sources/Components/ShootingComponent.swift
@@ -1,2 +1,34 @@
-import Foundation
+import GameplayKit
 
+public class ShootingComponent: GKComponent {
+    let origin: () -> CGPoint
+    let rateOfFire: TimeInterval = 0.6
+    var timeTillNextAttack:TimeInterval = 0
+
+    init(origin: @escaping () -> CGPoint) {
+        self.origin = origin
+        super.init()
+    }
+
+    public required convenience init?(coder: NSCoder) {
+        guard let origin = coder.decodeObject(forKey: "origin") as? () -> CGPoint else {
+                return nil
+        }
+        self.init(origin: origin)
+    }
+
+    public override func update(deltaTime seconds: TimeInterval) {
+        super.update(deltaTime: seconds)
+
+        timeTillNextAttack -= seconds
+        guard let entity = entity,
+            let target = entity.target,
+            timeTillNextAttack < 0 else {
+                return
+        }
+
+        timeTillNextAttack = rateOfFire
+
+        //Make a bullet entity and fire it
+    }
+}
