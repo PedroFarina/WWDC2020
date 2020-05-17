05dd1e09b09d1af889d8d24ca8bcd5c16e7fc34c
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-05-17 12:48:41 -0300

    Update Contents.swift

--- WWDC 2020.playground/Contents.swift
+++ WWDC 2020.playground/Contents.swift
@@ -1,12 +1,22 @@
-//: A SpriteKit based Playground
+//: A code organization Tower Defense Playground
 
+//#-hidden-code
 import PlaygroundSupport
 import SpriteKit
 
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 700))
+
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 700, height: 700))
 sceneView.presentScene(EntityManager.shared().scene)
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
+//#-end-hidden-code
 
+let numberOfBugs = /*#-editable-code Number of bugs*/30/*#-end-editable-code*/
+let spawnRate = /*#-editable-code Spawn rate*/0.5/*#-end-editable-code*/
 
-
+//#-hidden-code
+var spawner = SpawnerEntity(rateOfSpawn: spawnRate, numberOfBugs: numberOfBugs, path: EntityManager.shared().walkablePath) {
+    EntityManager.shared().noMoreBugs = true
+}
+EntityManager.shared().scene.spawner = spawner
+//#-end-hidden-code
